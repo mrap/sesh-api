19c52e411619af5fa6998e7943f2fbdc589a69a7
Michael Rapadas <mike@mrap.me>
2013-10-15 15:44:09 -0700

    Removes User in order to generate User scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 SeshApi::Application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -40,7 +39,7 @@ SeshApi::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
