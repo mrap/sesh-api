13831bcfba078b6a5c8b393a7530950a60ae9f70
Michael Rapadas <mike@mrap.me>
2013-10-18 17:56:22 -0700

    declares default_version. (DRYs it up)

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
+  default_version 1
 
   describe 'fetching a list of users' do
     before { 10.times { create(:user) } }
-    subject { get :index, version: 1 }
+    subject { get :index }
     it { should be_collection_resource }
   end
 
@@ -12,14 +13,14 @@ describe Api::V1::UsersController do
     let!(:user) { create(:user) }
 
     context 'with valid slug' do
-      subject { get :show, id: user.slug, version: 1 }
+      subject { get :show, id: user.slug}
       its(:status) { should eq 200 }
       it { should be_singular_resource }
       it { should have_exposed user }
     end
 
     context 'with invalid slug' do
-      subject { get :show, id: 'invalid-slug', version: 1 }
+      subject { get :show, id: 'invalid-slug'}
       its(:status) { should eq 404 }
       it { should have_exposed nil }
     end
