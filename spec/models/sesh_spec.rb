d2f42258003a2da929266d9138472c51f642f994
Michael Rapadas <mike@mrap.me>
2013-10-21 12:55:28 -0700

    Some spec cleanup

--- spec/models/sesh_spec.rb
+++ spec/models/sesh_spec.rb
@@ -1,17 +1,12 @@
 require 'spec_helper'
 
 describe Sesh do
-  before do
-    @sesh = create(:sesh)
-  end
-  subject { @sesh }
+  subject(:sesh) { create(:sesh) }
 
   it { should belong_to(:author) }
-
   its(:title)   { should_not be_nil }
   its(:author)  { should_not be_nil }
   it { should respond_to(:audio) }
-
   # Validations
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:author) }
