260ce9c6c0dfbe7b672136def2d9ae4d6552034d
Michael Rapadas <mike@mrap.me>
2013-10-25 00:30:42 -0700

    Tidies sesh.rb code and explicitly name field type: String

--- app/models/sesh.rb
+++ app/models/sesh.rb
@@ -3,14 +3,22 @@ class Sesh
   include Mongoid::Timestamps
   include Mongoid::Paperclip
 
+  # Relations
+  belongs_to :author, class_name: 'User', inverse_of: :seshes
+
+  # Fields
+  field :title,         type: String
+  field :is_anonymous,  type: Boolean,  default: false
+
+  # Assets
+  has_mongoid_attached_file :audio, dependent: :destroy
+
+  # Validations
+  validates_presence_of :author
+
+  # Named Scopes
   scope :recent, ->{ order_by(created_at: :desc) }
   scope :anonymous_only, ->{ where(is_anonymous: true)  }
-
-  field   :title
-  belongs_to :author, class_name: 'User', inverse_of: :seshes
-  field :is_anonymous, type: Boolean, default: false
-  has_mongoid_attached_file :audio, dependent: :destroy
-  validates_presence_of :author
 
   before_create :set_default_title
 
