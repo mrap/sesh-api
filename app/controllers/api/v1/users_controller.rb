f75dc42b14dab9303d022d8c98d6e643d226aab5
Michael Rapadas <mike@mrap.me>
2013-10-29 20:11:17 -0700

    Refactors users#show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,54 +1,39 @@
 class Api::V1::UsersController < ApplicationController
 
-  version 1
-
   before_action :get_user, only: [:show, :update, :destroy]
 
   # GET /users
-  # GET /users.json
   def index
     @users = User.all
-
-    collection json: @users
   end
 
   # GET /users/1
-  # GET /users/1.json
   def show
-    if correct_user?
-      expose  info: { username: @user.username },
-              seshes: @user.seshes
-    else
-      expose  info: { username: @user.username },
-              seshes: @user.public_seshes
-    end
+    authenticate_user!
+    # implicitly calls get_user
   end
 
   # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
     if @user.save
-      expose  info: { username: @user.username },
-              authentication_token: @user.authentication_token
+      @user
     else
-      error! :invalid_resource, @user.errors
+      render json: @user.errors, status: 422 # Unprocessable Entity
     end
   end
 
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     if @user.update(params[:user])
       head :no_content
     else
-      expose json: @user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     head :no_content
@@ -57,19 +42,18 @@ class Api::V1::UsersController < ApplicationController
   private
 
     def get_user
-      if @user = User.find(params[:id])
-        @user
-      else
-        error! :not_found
+      unless @user = User.find(params[:id])
+        render nothing: true, status: 404
+        return false
       end
+    end
+
+    def authenticate_user!
+      @user_authenticated = @user.authentication_token == params[:auth_token]
     end
 
     def user_params
       params.required(:user).permit(:username, :email, :password)
-    end
-
-    def correct_user?
-      @user.authentication_token == params[:authentication_token]
     end
 
 end
---
+++ spec/requests/api/v1/users/show_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "GET a user" do
+
+  before do
+    @user           = create(:user)
+    @sesh           = create(:sesh, author_id: @user.id)
+    @anonymous_sesh = create(:anonymous_sesh, author_id: @user.id)
+    get "/api/v1/users/#{@user.slug}"
+
+  end
+
+  it 'is successful' do
+    response.status.should eq 200
+  end
+
+  it 'exposes correct user info' do
+    @info = json['info']
+    @info['username'].should match @user.username
+  end
+
+  it 'exposes non-anonymous seshes only' do
+    json['seshes'].length.should eq 1
+  end
+
+  context 'when requested with valid auth token' do
+    before do
+      get "/api/v1/users/#{@user.slug}", auth_token: @user.authentication_token
+    end
+
+    it 'exposes all user seshes (includes anonymous)' do
+      json['seshes'].length.should eq 2
+    end
+  end
+
+  context 'when user not found' do
+    before { get "/api/v1/users/invalid-slug" }
+
+
+    it 'is not found' do
+      response.status.should eq 404
+    end
+  end
+end
